a4de9cfcc1923c9ace23f7f5995786f5ab25cea7
Agus2309 <agus.cavia.3@gmail.com>
2024-03-11 16:48:41 -0300

    Design changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,60 @@
-
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Technologies from "./components/Technologies";
 import Projects from "./components/Projects";
 import About from "./components/About";
-import Contact from "./components/Contact";
+import Contact from "./components/contact";
 import { Element } from "react-scroll";
 import ScrollAnimation from 'react-animate-on-scroll';
+import { useEffect, useState } from "react";
+import { FaJava } from "react-icons/fa";
+import { MdJavascript, MdPhp } from "react-icons/md";
 
+const welcomes = ["Welcome!", "Bienvenido!", "Bem-vindo!", "Benvenuto!", "Willkommen!"];
+const icons = [<FaJava/>, <MdJavascript/>, <MdPhp/> ]
 const name = 'Jorge';
 
 function App() {
+  const [currentLanguageIndex, setCurrentLanguageIndex] = useState(0);
+  const [currentIconIndex, setCurrentIconIndex] = useState(0);
+
+  useEffect(() => {
+    const languageInterval = setInterval(() => {
+      setCurrentLanguageIndex(prevIndex => (prevIndex + 1) % welcomes.length);
+    }, 3000);
+
+    const iconInterval = setInterval(() => {
+      setCurrentIconIndex(prevIndex => (prevIndex + 1) % icons.length);
+    }, 5000);
+
+    return () => {
+      clearInterval(languageInterval);
+      clearInterval(iconInterval);
+    };
+  }, []);
+
   return (
     <>
       <div className="min-h-screen min-w-full">
         <Element name="home" id="home">
           <div
-            className="bg-cover bg-center pb-20 "
+            className="bg-cover bg-center pb-20"
             style={{
               height: '100vh',
-              backgroundImage: `url('/background.jpg')`,
+              backgroundImage: `url('/background2.jpg')`,
             }}
           >
             <Navbar/>
             <section className="mt-24 mb-24">
-              <div className="ml-8 text-white">
-                <h1 className="text-4xl lg:text-8xl md:text-8xl sm:text-8xl xxs:text-8xl">Welcome!</h1>
-                <p className="text-2xl">I'm {name}</p>
+              <div className="ml-8 text-white flex">
+                <div>
+                  <h1 className="text-4xl lg:text-8xl md:text-8xl sm:text-8xl xxs:text-8xl font-semibold">{welcomes[currentLanguageIndex]}</h1>
+                  <p className="text-2xl">I'm {name},</p>
+                  <p className="text-lg">a Fullstack Web Developer</p>
+                </div>
+                <div className="ml-2 text-9xl">
+                  {icons[currentIconIndex]}
+                </div>
               </div>
             </section>
           </div>
